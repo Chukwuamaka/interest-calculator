d16e86d8d14777afebc305e3190ee48ff3e91f56
Chukwuamaka <osujichukwuamaka@gmail.com>
2020-09-16 07:15:03 +0100

    Make page responsive and refactor Typescript code

--- index.ts
+++ index.ts
@@ -1,69 +1,68 @@
-interface Duration {
-    duration: number,
-    durationBasis: string
-}
-
 interface Rate {
     rate: number,
     rateBasis: string
 }
 
-const simpleInterest = (principal: number, rate: Rate, time: Duration): number => {
+const simpleInterest = (principal: number, rate: Rate, time: number): number => {
+    const formula: number = (principal * rate.rate * time) / 100;
     let result: number;
+    
     if (rate.rate === 0) {
         result = 0;
     }
-    else if (rate.rateBasis === time.durationBasis) {
-        result = (principal * rate.rate * time.duration) / 100;
-    }
-    else if (rate.rateBasis === 'month' && time.durationBasis === 'year') {
-        result = (principal * rate.rate * time.duration * 12) / 100;
-    }
-    else {
-        result = (principal * rate.rate * time.duration / 12) / 100;
+
+    switch (rate.rateBasis) {
+        case 'month':
+            result = formula;
+            break;
+        
+        case 'year':
+            result = formula / 12;
     }
     
     return result;
 }
 
-const compoundInterestAmount = (principal: number, rate: Rate, time: Duration): number => {
+const compoundInterestAmount = (principal: number, rate: Rate, time: number): number => {
+    let exponent: number = time;
+    const formula = principal * Math.pow((1 + rate.rate/100), exponent);
     let result: number;
+
     if (rate.rate === 0) {
         result = principal;
     }
-    else if (rate.rateBasis === time.durationBasis) {
-        result = principal * Math.pow((1 + rate.rate/100), time.duration);
-    }
-    else if (rate.rateBasis === 'month' && time.durationBasis === 'year') {
-        result = principal * Math.pow((1 + rate.rate/100), (time.duration * 12));
-    }
-    else {
-        result = principal * Math.pow((1 + rate.rate/100), (time.duration / 12));
+
+    switch (rate.rateBasis) {
+        case 'month':
+            result = formula;
+            break;
+
+        case 'year':
+            exponent = time / 12;
+            result = formula;
     }
     
-    return Number(result.toFixed(2));
+    return Number(result.toFixed(2)); //toFixed() method returns a string but 'result' is of type 'number', hence the conversion.
 }
 
-document.getElementById("button").addEventListener("click", (event) => {
-    // event.preventDefault();
+document.getElementById("button").addEventListener("click", () => {
+    // Data from forms are collected as string, but some properties are of type 'number', hence the conversion in some cases.
     let interestType: string = (document.getElementById("interest-type") as HTMLSelectElement).value;
     let principal: number = Number((document.getElementById("amount") as HTMLInputElement).value);
     let rate: number = Number((document.getElementById("rate") as HTMLInputElement).value);
     let rateBasis: string = (document.getElementById("rate-basis") as HTMLSelectElement).value;
     let duration: number = Number((document.getElementById("duration") as HTMLInputElement).value);
-    let durationBasis: string = (document.getElementById("duration-basis") as HTMLSelectElement).value;
-
     let interest: number, totalAmount: number;
+    
     switch (interestType) {
         case "compound":
-            totalAmount = compoundInterestAmount(principal, {rate, rateBasis}, {duration, durationBasis}); 
+            totalAmount = compoundInterestAmount(principal, {rate, rateBasis}, duration); 
             interest = totalAmount - principal;
             break;
     
         default:
-            interest = simpleInterest(principal, {rate, rateBasis}, {duration, durationBasis});
+            interest = simpleInterest(principal, {rate, rateBasis}, duration);
             totalAmount = principal + interest;
-            break;
     }
 
     document.getElementById("principal").innerHTML = principal.toString();
--- src/index.js
+++ src/index.js
@@ -1,54 +1,52 @@
 "use strict";
 var simpleInterest = function (principal, rate, time) {
+    var formula = (principal * rate.rate * time) / 100;
     var result;
     if (rate.rate === 0) {
         result = 0;
     }
-    else if (rate.rateBasis === time.durationBasis) {
-        result = (principal * rate.rate * time.duration) / 100;
-    }
-    else if (rate.rateBasis === 'month' && time.durationBasis === 'year') {
-        result = (principal * rate.rate * time.duration * 12) / 100;
-    }
-    else {
-        result = (principal * rate.rate * time.duration / 12) / 100;
+    switch (rate.rateBasis) {
+        case 'month':
+            result = formula;
+            break;
+        case 'year':
+            result = formula / 12;
     }
     return result;
 };
 var compoundInterestAmount = function (principal, rate, time) {
+    var exponent = time;
+    var formula = principal * Math.pow((1 + rate.rate / 100), exponent);
     var result;
     if (rate.rate === 0) {
         result = principal;
     }
-    else if (rate.rateBasis === time.durationBasis) {
-        result = principal * Math.pow((1 + rate.rate / 100), time.duration);
+    switch (rate.rateBasis) {
+        case 'month':
+            result = formula;
+            break;
+        case 'year':
+            exponent = time / 12;
+            result = formula;
     }
-    else if (rate.rateBasis === 'month' && time.durationBasis === 'year') {
-        result = principal * Math.pow((1 + rate.rate / 100), (time.duration * 12));
-    }
-    else {
-        result = principal * Math.pow((1 + rate.rate / 100), (time.duration / 12));
-    }
-    return Number(result.toFixed(2));
+    return Number(result.toFixed(2)); //toFixed() method returns a string but 'result' is of type 'number', hence the conversion.
 };
-document.getElementById("button").addEventListener("click", function (event) {
-    // event.preventDefault();
+document.getElementById("button").addEventListener("click", function () {
+    // Data from forms are collected as string, but some properties are of type 'number', hence the conversion in some cases.
     var interestType = document.getElementById("interest-type").value;
     var principal = Number(document.getElementById("amount").value);
     var rate = Number(document.getElementById("rate").value);
     var rateBasis = document.getElementById("rate-basis").value;
     var duration = Number(document.getElementById("duration").value);
-    var durationBasis = document.getElementById("duration-basis").value;
     var interest, totalAmount;
     switch (interestType) {
         case "compound":
-            totalAmount = compoundInterestAmount(principal, { rate: rate, rateBasis: rateBasis }, { duration: duration, durationBasis: durationBasis });
+            totalAmount = compoundInterestAmount(principal, { rate: rate, rateBasis: rateBasis }, duration);
             interest = totalAmount - principal;
             break;
         default:
-            interest = simpleInterest(principal, { rate: rate, rateBasis: rateBasis }, { duration: duration, durationBasis: durationBasis });
+            interest = simpleInterest(principal, { rate: rate, rateBasis: rateBasis }, duration);
             totalAmount = principal + interest;
-            break;
     }
     document.getElementById("principal").innerHTML = principal.toString();
     document.getElementById("interest").innerHTML = interest.toString();
